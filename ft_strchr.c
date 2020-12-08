99e61e6123a4a7ea5d6a19f43c8fdc2bd8d567ff
tmoragli <tmoragli@student.42.fr>
2020-12-08 17:58:30 +0100

    strchr fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:32:10 by telli             #+#    #+#             */
-/*   Updated: 2020/12/08 17:57:26 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/08 17:58:23 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *s, int c)
 	int i;
 
 	i = 0;
-	while (s[i] != c || s[i])
+	while (s[i] != c && s[i])
 		i++;
 	if (c == s[i])
 		return ((char *)(&s[i]));
