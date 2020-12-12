a675a4f8ea4907d9e53c0e7dddee220749f0c4ed
tmoragli <tmoragli@student.42.fr>
2020-12-12 20:04:37 +0100

    substr fix

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 12:55:33 by telli             #+#    #+#             */
-/*   Updated: 2020/12/12 20:02:33 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/12 20:04:24 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t i;
 	
 	i = 0;
+	if (!s)
+		return NULL;
 	if (!(dest = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	while (i < len)
