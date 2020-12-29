54367cbea6ce90c75197b3111f6c3ad55bf79b84
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:09:08 +0100

    memchr fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 00:57:29 by telli             #+#    #+#             */
-/*   Updated: 2020/12/29 18:08:12 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:09:05 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	temp = (unsigned char *)s;
 	while	(n--)
 	{
-		if	(*temp == (unsigned char)c))
+		if	(*temp == (unsigned char)c)
 			return (temp);
 		temp++;
 	}
