d4d43a5f12039c1a535ebd211aa9bc50511e6175
tmoragli <tmoragli@student.42.fr>
2020-12-29 19:03:17 +0100

    strlcat fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,22 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:44:13 by telli             #+#    #+#             */
-/*   Updated: 2020/12/29 19:02:14 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 19:03:12 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, unsigned int size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t i;
 	size_t j;
 
 	i = 0;
-	while (dest[i] && i < n)
+	while (dest[i] && i < size)
 		i++;
 	j = i;
-	while (src[i - j] && i < n- 1)
+	while (src[i - j] && i < size - 1)
 	{
 		src[i] = src[i - j];
 		i++;
