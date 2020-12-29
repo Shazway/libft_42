d1f32cd839cd62cb79f940ac22d6224e532c5b5f
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:24:49 +0100

    strrchr fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:35:19 by telli             #+#    #+#             */
-/*   Updated: 2020/12/29 18:23:18 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:24:47 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	size_t n;
 
 	n = ft_strlen(s);
-	if ((char)c) == '\0')
+	if ((char)c == '\0')
 		return ((char *)s + n);
 	while (n--)
 		if (*(s + n) == c)
