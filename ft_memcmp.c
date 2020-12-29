52f12e7ce0d3a5726821b2f4b7cbabb61e82713b
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:15:51 +0100

    memcmp fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:02:50 by telli             #+#    #+#             */
-/*   Updated: 2020/12/07 19:26:24 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:15:25 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
 	unsigned char *temp1;
 	unsigned char *temp2;
 
+	if (s1 == s2 || n == 0)
+		return (0);
 	temp1 = (unsigned char *)s1;
 	temp2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	i = -1;
-	while ((temp1[++i] && temp2[i]) && i < n)
-		if (temp1[i] != temp2[i])
-			return (temp2[i] - temp1[i]);
-	return (temp2[i] - temp1[i]);
+	while (n--)
+	{
+		if (*temp1 != *temp2)
+			return (*temp1 - *temp2);
+		temp1++;
+		temp2++;
+	}
+	return (0);
 }
