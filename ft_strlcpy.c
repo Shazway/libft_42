7e16394b33e6c7e52e89cdae9cf36993e28e2baf
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:45:07 +0100

    strrchr fix

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,19 +6,29 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 20:21:34 by telli             #+#    #+#             */
-/*   Updated: 2020/12/13 07:11:58 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:45:04 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strlcpy(char *dest, const char *src)
+char *ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	int i;
+	unsigned int i;
 
-	i = -1;
-	while(src[++i])
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
+	i = 0;
+	if (!dest || !src)
+		return (NULL);
+	if (size > 0)
+	{
+		while (--size && src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	while (dest (src[i]))
+		i++;
+	return (i);
 }
