2caa0cfa88c13dc2f4b2d84e603750447540b3e4
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:30:12 +0100

    strrchr fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:39:33 by telli             #+#    #+#             */
-/*   Updated: 2020/12/29 18:28:29 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:30:09 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 
-	i = -1;
+	i = 0;
 	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
