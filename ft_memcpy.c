40debf029fc9434de828a7fd1bfe82447ea93d74
tmoragli <tmoragli@student.42.fr>
2020-12-29 17:34:50 +0100

    memcpy fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 00:21:17 by telli             #+#    #+#             */
-/*   Updated: 2020/12/07 18:58:57 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 17:34:34 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	unsigned char *cast_dest;
+	unsigned char *cast_dst;
 	unsigned char *cast_src;
 
-	cast_dest = (unsigned char *)dest;
+	cast_dst = (unsigned char *)dest;
 	cast_src = (unsigned char *)src;
-	i = -1;
-	while (cast_src[++i] && i < n)
-		cast_dest[i] = cast_src[i];
-	return (cast_dest);
+	if	(!n || dest == src)
+		return (dest);
+	while (n)
+	{
+		*cast_dest++ = *cast_str++;
+		n--
+	}
+	return (dest);
 }
