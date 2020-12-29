af858e709756ab31ce4d2218e154c1910fde22f0
tmoragli <tmoragli@student.42.fr>
2020-12-29 18:53:31 +0100

    strlcpy fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:44:13 by telli             #+#    #+#             */
-/*   Updated: 2020/12/07 19:34:42 by tmoragli         ###   ########.fr       */
+/*   Updated: 2020/12/29 18:53:26 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ size_t	ft_strlcat(char *dest, const char *src, unsigned int size)
 	unsigned int i;
 	unsigned int j;
 
-	j = -1;
+	j = 0;
 	i = 0;
 	while(dest[i])
 		i++;
-	while (src[++j] && j < size)
+	while (src[j] && j < size - 1)
+	{
 		dest[i + j] = src[j];
+		j++;
+	}
 	dest[i + j] = '\0';
 	i = 0;
 	while (src[i])
