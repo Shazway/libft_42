3567e8efdbc4a52e6555cfc7b2129b437cc7f669
tmoragli <tmoragli@student.42.fr>
2021-01-09 02:45:59 +0100

    fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:44:13 by telli             #+#    #+#             */
-/*   Updated: 2021/01/09 02:34:56 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/09 02:45:53 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t j;
 
 	i = 0;
+	if	(size == 0)
+		return (0);
 	while (dest[i] && i < size)
 		i++;
 	j = i;
