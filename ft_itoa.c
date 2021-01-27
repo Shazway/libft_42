c360f66defa6fc12dd6672984a61f4dab0698c2b
tmoragli <tmoragli@student.42.fr>
2021-01-27 16:27:12 +0100

    norm changes

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 19:07:49 by telli             #+#    #+#             */
-/*   Updated: 2021/01/27 16:24:48 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:27:07 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,8 @@ char	*ft_allocate_array(long int n, char *dest)
 	if (!(dest = malloc(sizeof(char) * (count + 1))))
 		return (NULL);
 	dest[count] = '\0';
-	dest = ft_fill_array(temp, dest, count--);
-	return (dest);
+	count--;
+	return (ft_fill_array(temp, dest, count));
 }
 
 char	*ft_itoa(int n)
