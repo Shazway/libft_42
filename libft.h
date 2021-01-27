eef132ff652f71cec033f8185fcb666dd0cce5ab
tmoragli <tmoragli@student.42.fr>
2021-01-27 16:37:05 +0100

    norm changes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 01:10:22 by telli             #+#    #+#             */
-/*   Updated: 2021/01/27 16:36:04 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:37:02 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,8 @@ void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstadd_front(t_list **alst, t_list *new);
 int					ft_lstsize(t_list *lst);
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstmap(t_list *lst,
+					void *(*f)(void *), void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
