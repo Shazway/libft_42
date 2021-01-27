c2ef471bbdc76171746579415527e02557730a1e
tmoragli <tmoragli@student.42.fr>
2021-01-27 14:02:10 +0100

    fix/add

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 13:41:06 by tmoragli          #+#    #+#             */
-/*   Updated: 2021/01/27 14:00:38 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/27 14:02:07 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ int	ft_lstsize(t_list *lst)
 {
 	int i;
 
-	i = 1;
+	i = 0;
 	while (lst->next)
 	{
 		lst = lst->next;
 		i++;
 	}
+	if (lst->next == NULL)
+		i++;
 	return (i);
 }
