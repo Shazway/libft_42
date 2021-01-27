ac926c85571d3faebe374ab3194f097f50b0b427
tmoragli <tmoragli@student.42.fr>
2021-01-27 15:36:23 +0100

    fix/add

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 15:04:46 by tmoragli          #+#    #+#             */
-/*   Updated: 2021/01/27 15:34:13 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/27 15:36:21 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list    *dest;
 	t_list    *temp;
 
-	while (list)
+	while (lst)
 	{
-        if (!(temp = ft_lstnew(f(list->content))))
+        if (!(temp = ft_lstnew(f(lst->content))))
         {
             ft_lstclear(&dest, del);
             return (NULL);
         }
         ft_lstadd_back(&dest, temp);
-        list = list->next;
+        lst = lst->next;
     }
     return (dest);
 }
