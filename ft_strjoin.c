275003048ad5d05a8b518534f2325c33f8282d49
tmoragli <tmoragli@student.42.fr>
2021-01-27 16:12:56 +0100

    norm changes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 13:22:35 by telli             #+#    #+#             */
-/*   Updated: 2021/01/27 16:11:53 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:12:52 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = ft_strlen((void *)s1) + ft_strlen((void *)s2) + 1;
 	if (!(dest = malloc(sizeof(char) * (i))))
 		return (NULL);
-	j = 0;
-	while (s1[j++])
-		dest[j] = s1[j];
+	i = 0;
+	while (s1[i++])
+		dest[i] = s1[i];
+	j = i;
 	i = 0;
 	while (s2[i])
 	{
