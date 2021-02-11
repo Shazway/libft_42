c27930813ff415f53eec3da08116fe08d81e41ae
tmoragli <tmoragli@student.42.fr>
2021-02-11 23:42:20 +0100

    Strlcat redo

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 01:44:13 by telli             #+#    #+#             */
-/*   Updated: 2021/01/27 15:53:04 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/02/11 23:41:00 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t i;
-	size_t j;
+	size_t lendst;
+	size_t lensrc;
 
 	i = 0;
-	if (size == 0)
-		return (0);
-	while (dest[i] && i < size)
-		i++;
-	j = i;
-	while (src[i - j] && i < size - 1)
+	lendst = ft_strlen(dest);
+	lensrc = ft_strlen(src);
+	if (size - 1 <= lendst)
+		return (lensrc + size);
+	while (lendst + i < size - 1)
 	{
-		dest[i] = src[i - j];
+		dest[lendst + i] = src[i];
 		i++;
 	}
-	if (j < size)
-		dest[i] = '\0';
-	return (j + ft_strlen(src));
+	dest[lendst + i] = '\0';
+	return (lendst + lensrc);
 }
